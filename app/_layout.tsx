942862d3f9e9925c658875d73443dd181fc7a6e6
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-07-18 11:56:14 -0400

    [navigation] Add bottom Tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack>
-    {/* stack.screen es el titulo de la vista */}
-    <Stack.Screen name="index" options={{ title: 'Home' }} />
-    <Stack.Screen name="about" options={{ title: 'About' }} />
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
