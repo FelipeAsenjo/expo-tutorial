5f2c5e9bee5093c1f8dfd882f34ae7013fb2f41d
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-07-19 16:42:29 -0400

    [modal] Display modal when pressing button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from 'expo-image-picker';
@@ -11,6 +12,7 @@ const PlaceholderImage = require('@/assets/images/background-image.png')
 export default function Index() {
   const [selectedImage, seteSelectedImage] = useState<string | undefined>(undefined)
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -32,11 +34,15 @@ export default function Index() {
   };
 
   const onAddSticker = () => {
-    // we will implement this later
+    setIsModalVisible(true)
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false)
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    // implement later
   };
 
   return (
@@ -60,6 +66,9 @@ export default function Index() {
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* Emoji list component will go here */}
+      </EmojiPicker>
     </View>
   );
 }
