94f29aa52bf7b84954d53592dd0622cbd68642b2
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-07-21 00:41:49 -0400

    [gesture - animation] Apply gestures with animations to stickers

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,5 +1,6 @@
-import { Image } from 'expo-image';
-import { ImageSourcePropType, View } from 'react-native';
+import { ImageSourcePropType } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 type Props = {
   imageSize: number;
@@ -7,9 +8,44 @@ type Props = {
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  // useSharedValue helps to mutate data and runs animations based on the current value.
+  // We can access and modify the shared value using the .value property.
+  const scaleImage = useSharedValue(imageSize)
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  // animated style
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      // withSpring uses spring real physics to animate the transitions
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
+  // define a gesture
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
-    </View>
+    <Animated.View style={{ top: -350 }}>
+      {/* Detect desirable gesture */}
+      <GestureDetector gesture={ doubleTap }>
+        {/* Animated components use the components styles to determine which values to animate */}
+        {/* Animated exports Animated.View, Animated.Text, Animated.ScrollView among others */}
+        <Animated.Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, {width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
+    </Animated.View>
   );
 }
